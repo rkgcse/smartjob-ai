a6ecf6df38ea1f70b58a3a5de93de73f7de972ad
rkgcse <raushancse001@gmail.com>
2026-01-30 01:35:45 +0530

    Use MongoDB Atlas

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
-mongoose.connect("mongodb://127.0.0.1:27017/smartjob");
+mongoose.connect(process.env.MONGO_URL);
 
 app.listen(5000, () => {
   console.log("Backend running on port 5000");
