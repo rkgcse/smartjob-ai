ad764d94210bfd0aa194c5cc452e9f398d94d409
rkgcse <raushancse001@gmail.com>
2026-01-31 17:45:01 +0530

    fixed routing

--- src/main.tsx
+++ src/main.tsx
@@ -1,27 +1,31 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
+
 import Login from "./pages/Login";
 import Register from "./pages/Register";
 import Dashboard from "./pages/Dashboard";
+import Upload from "./pages/Upload";
+
 import AdminLogin from "./pages/AdminLogin";
 import AdminDashboard from "./pages/AdminDashboard";
 
-function App() {
-  return (
+ReactDOM.createRoot(document.getElementById("root")!).render(
+  <React.StrictMode>
     <BrowserRouter>
       <Routes>
 
-        {/* PUBLIC */}
+        {/* USER ROUTES */}
         <Route path="/" element={<Login />} />
         <Route path="/register" element={<Register />} />
         <Route path="/dashboard" element={<Dashboard />} />
+        <Route path="/upload" element={<Upload />} />
 
-        {/* ADMIN */}
+        {/* ADMIN ROUTES */}
         <Route path="/admin" element={<AdminLogin />} />
         <Route path="/admin/dashboard" element={<AdminDashboard />} />
 
       </Routes>
     </BrowserRouter>
-  );
-}
-
-export default App;
+  </React.StrictMode>
+);
